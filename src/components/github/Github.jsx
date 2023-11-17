92edf30de6b7fe1d1f6ce3f27ce565ccd8332218
Antu Paul <antupaul81@gmail.com>
2023-11-17 17:32:03 +0600

    fetch data from github api

--- src/components/github/Github.jsx
+++ src/components/github/Github.jsx
@@ -5,18 +5,24 @@ function Github() {
 
 
     useEffect(()=> {
-        fetch(`https://api.github.com/user/paulantu`)
+        fetch(`https://api.github.com/users/paulantu`)
         .then((response) => response.json())
         .then((data) => setData(data))
     }, [])
 
-
+  
 
 
 
   return (
-    <div>
-        <div>Github Follower : </div>
+    <div className='text-center py-11'>
+        <div className='flex justify-center'>
+          <img src={data.avatar_url} alt='Github Profile picture' width={150}/>
+        </div>
+        <div className='mt-6'>
+          <h1 className='text-orange-500 text-2xl'>Name: {data.name}</h1>
+          <div>Github Follower : {data.followers}</div>
+        </div>
         <img src='' alt='' />
     </div>
   )
